791e9f855a541f84c785b495f7fee92f10852681
tandemdude <olijdthompson@gmail.com>
2019-04-24 18:00:42 +0100

    Working

--- ThomBot.py
+++ ThomBot.py
@@ -1,17 +1,18 @@
 import discord
 
-client = discor.client()
+client = discord.Client()
 
 @client.event
 async def on_ready():
 	print("The bot is ready!")
-	await client.change_presence(game=discord.Game(name="Making a bot"))
+	await client.change_presence(activity=discord.Game(name="Making a bot"))
 
 @client.event
 async def on_message(message):
 	if message.author == client.user:
 		return
-	if message.content == "Hello":
-		await client.send_message(message.channel, "World")
 
-client.run(NTcwMzU4NDkwOTkyNzM4MzE0.XL-B4w.Jp79yU5v34rXJX1sv13KqleIWAo)
+	if message.content == "Hello":
+		await message.channel.send("Hello!")
+
+client.run('NTcwMzU4NDkwOTkyNzM4MzE0.XL-B4w.Jp79yU5v34rXJX1sv13KqleIWAo')
