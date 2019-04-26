31aa4de68f4c076aa07f11f417ca97b22a9ccd33
tandemdude <olijdthompson@gmail.com>
2019-04-26 21:46:33 +0100

    More commands added

--- ThomBot.py
+++ ThomBot.py
@@ -1,11 +1,11 @@
 import discord
 import asyncio
+from datetime import datetime
 from discord.ext import commands
 
-user_display_name = ''
-user_avatar_url = ''
+prefix = '$'
 
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix=prefix)
 bot.remove_command('help')
 
 @bot.event
@@ -22,11 +22,25 @@ async def on_message(message):
 async def help(ctx):
 	embed = discord.Embed(title='Commands:', color=0x0080ff)
 	embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
-	embed.add_field(name='$say <message>', value=('Makes bot repeat message'), inline=True)
+	embed.add_field(name=f'{prefix}help', value=('Displays this message.'), inline=True)
+	embed.add_field(name=f'{prefix}say <message>', value=('Makes bot say message'), inline=True)
+	embed.add_field(name=f'{prefix}noot', value=('Noot Noot!'), inline=True)
+	embed.add_field(name=f'{prefix}time', value=('Displays UK time'), inline=False)
 	await ctx.send(embed=embed)
 
 @bot.command()
 async def say(ctx, *, message):
 	await ctx.send(message)
+
+@bot.command()
+async def noot(ctx):
+	await ctx.send('Noot Noot!')
+
+@bot.command()
+async def time(ctx):
+	current_time = datetime.today().strftime('%H:%M:%S')
+	embed = discord.Embed(color=0x0080ff)
+	embed.add_field(name=':alarm_clock: Time:', value=(f'Current UK time is: {current_time}'), inline=True)
+	await ctx.send(embed=embed)
 
 bot.run('')
